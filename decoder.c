ce6483878dbd0f0ba05d68b117610c892f7d01ae
jprobasco <jacobprobasco@outlook.com>
2015-11-30 11:42:26 -0500

    Add read to structure and print to screen from structure.

--- decoder.c
+++ decoder.c
@@ -62,8 +62,8 @@ struct IP_hdr {
     unsigned char ip_protocol[1];
                 // 11 = UDP
     unsigned char ip_chksum[2];
-    unsigned char ip_srce_pt[2];
-    unsigned char ip_dest_pt[2];
+    unsigned char ip_srce_ip[4];
+    unsigned char ip_dest_ip[4];
 }ip_frame;
 
 // UDP Header
@@ -74,59 +74,60 @@ struct UDP_frame {
     unsigned char udp_chksum[2];
 }udp_frame;
 
+// Meditrik header. - Maximum size of med_header is 24B
+struct MED_hdr {
+// Double-check after conversion
+    unsigned int med_version:4;
+// Double-check after conversion
+    unsigned int med_squence:9;
+// Double-check after conversion
+    unsigned int med_type:3;
+    unsigned char med_length[4];
+    unsigned char med_srce_dev[8];
+    unsigned char med_dest_dev[8];
+}med_head;
+
 // Meditrik Variable Portion - Will be one of the following
-struct MED_frame {
-    // Meditrik header. - Maximum size of med_header is 24B
-    struct MED_hdr {
-        // Double-check after conversion
-        unsigned int med_version:4;
-        // Double-check after conversion
-        unsigned int med_squence:9;
-        // Double-check after conversion
-        unsigned int med_type:3;
-        unsigned char med_length[4];
-        unsigned char med_srce_dev[8];
-        unsigned char med_dest_dev[8];
-    }med_head;
-    
-    struct MED_payload {
-        /// 0 - Device Status - 28B
-        struct MED_stat{
-            unsigned char med_stat_batt[16];
-            // IEEE 754 double-precision decimal (binary64)
-            unsigned char med_stat_gluc[4];         // 0-65000
-            unsigned char med_stat_caps[4];         // 0-65000
-            unsigned char med_stat_omor[4];         // 0-65000
-        }med_stat;
-        
-        /// 1 - Command Instruction - 8B
-        struct MED_cmd{
-            unsigned char cmd_out[4];
+struct MED_payload {
+
+    /// 0 - Device Status - 28B
+    struct MED_stat{
+        unsigned char med_stat_batt[16];
+                // IEEE 754 double-precision decimal (binary64)
+        unsigned char med_stat_gluc[4];         // 0-65000
+        unsigned char med_stat_caps[4];         // 0-65000
+        unsigned char med_stat_omor[4];         // 0-65000
+    }med_stat;
+
+    /// 1 - Command Instruction - 8B
+    struct MED_cmd{
+        unsigned char cmd_out[4];
             // Sends command to device
             /// GET: STATUS(0), GPS(2)
             /// SET: GLUSCOSE(1), CAPSACIAN(3), OMORFINE(5)
             /// REPEAT(7)
             /// RESERVED(4, 6)
-            unsigned char cmd_param[4];
+        unsigned char cmd_param[4];
             // Parameters for given SET Commands
-        }med_cmd;
-        
-        /// 2 - GPS Data - 40B
-        struct MED_gps{
-            unsigned char longi[16];
+    }med_cmd;
+
+    /// 2 - GPS Data - 40B
+    struct MED_gps{
+        unsigned char longi[16];
             // binary64 - degrees, can be negative
-            unsigned char latit[16];
+        unsigned char latit[16];
             // binary64 - degrees, can be negative
-            unsigned char altit[8];
+        unsigned char altit[8];
             // binary32
-        }med_gps;
-        
-        /// 3 - Message
-// INCORRECT. Must be med_length minus 32 (for the med_header)
+    }med_gps;
+
+    /// 3 - Message
+    struct MED_mess{
+// INCORRECT. Must be med_length - 32 (for the med_header)
         unsigned char message;
             // NOT NULL-terminated
-    }med_payload;
-}med_frame;
+    }med_message;
+};
 
 int main(void){
     /*
@@ -134,7 +135,7 @@ int main(void){
     extern int errno;                  // Error handling
     int error_n;                        // Place-holder, error number
     char secr_word[36] = { '\0' };      // Place-holder, secret word
-    
+
     // Manage optional command-line arguments
     if(argc > 2){                       // Check for more than one argument, error.
         error_n = errno;
@@ -153,41 +154,61 @@ int main(void){
     struct stat pcap_stat;                                  // For getting pcap file information
     long long pcap_size;
     int prnt_head(unsigned char *buffer, int buff_size);    // For printing the data to screen
-    
+
     FILE *pcap;
-    pcap = fopen("/codec/pcaps/hello.pcap", "rb");  // Open file as a data stream
-    
+    pcap = fopen("/usr/local/share/codec/hello.pcap", "rb");  // Open file as a data stream
+
     int pcap_fileno;                 				// Locate file number.
     pcap_fileno = fileno(pcap);                 	// Locate file number.
     fstat(pcap_fileno, &pcap_stat);                 // Load File's stats into pcap_stat.
     pcap_size = pcap_stat.st_size;                  // Set pcap_size to the size of the file.
-    
+
     unsigned char buff[pcap_size];                  // Create buffer for a file of that size.
     memset(buff, 0, pcap_size);
-    
-    printf("Printing Whole PCAP\n");
-    fread(buff, pcap_size, 1, pcap);
-    
-    prnt_head(buff, (int)pcap_size);                // Print whole PCAP
+
+    fread(&global_pcap_head, sizeof(global_pcap_head), 1, pcap);
+    fread(&packet_head, sizeof(packet_head), 1, pcap);
+	fread(&eth_frame, sizeof(eth_frame), 1, pcap);
+    fread(&ip_frame, sizeof(ip_frame), 1, pcap);	
+    fread(&udp_frame, sizeof(udp_frame), 1, pcap);
+
+    printf("Print Global PCAP Header\n");
+	prnt_head((unsigned char *)&global_pcap_head, sizeof(global_pcap_head));                // Print global Header
+	
+    printf("Print packet header\n");
+	prnt_head((unsigned char *)&packet_head, sizeof(packet_head));                // Print packet header
+
+	printf("Print ethernet frame\n");
+	prnt_head((unsigned char *)&eth_frame, sizeof(eth_frame));		// Print Ethernet frame
+	
+    printf("Print IP Frame\n");
+	prnt_head((unsigned char *)&ip_frame, sizeof(ip_frame));                // Print ip frame
+	
+    printf("Print UDP Frame\n");
+	prnt_head((unsigned char *)&udp_frame, sizeof(udp_frame));                // Print udp frame
+
+
+    printf("Print Magic_number\n");
+		printf("\n%02x\n",(unsigned int)*global_pcap_head.glbl_magic_num);
     printf("\n\n");
-    
-    
-    
-    
+
+
+
+
 /* EXAMPLE BIT-Masking for Flags
- 
+
     char flags = 0xFB; // 11111011b char mask = 0x01; // 00000001b
     if(flags & mask){
         printf("Flag 1 set!\n");
     }
  */
 
-    
+
 
 
 //  REFERENCE.   // buffer   // each elem    // numb elements    // file
 //  size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
-    
+
 
 }
 
@@ -195,7 +216,7 @@ int prnt_head(unsigned char* buffer, int buff_size){
     printf("\n\n");
 
     int count = 0;
-    
+
     for(int i = 0; i < buff_size; i++){
         printf("%02X ",buffer[i]);
         if (count == 15){
