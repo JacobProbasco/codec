440fde5d842877c867c9e6693f76b4270cff5936
JacobProbasco <jacobprobasco@gmail.com>
2015-12-03 05:26:40 -0500

    Initial add encoder.c

---
+++ encoder.c
@@ -0,0 +1,16 @@
+//  codec
+//  encoder.c
+//
+//  Created by Jacob Probasco on 12/3/15.
+//  Copyright © 2015 jprobasco. All rights reserved.
+//
+//  And now for some fun with mostly non-delimited text!
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    printf("I can make PCAP files like the best of them. Just wait and see.\n");
+    
+    return 0;
+}
