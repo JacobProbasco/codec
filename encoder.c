3dceaee048de7c25606c5e116dfee541c4ae6384
JacobProbasco <jacobprobasco@gmail.com>
2015-12-03 09:32:51 -0500

    Simplify argument checking.

--- encoder.c
+++ encoder.c
@@ -13,7 +13,8 @@
 #include <unistd.h>         // strerror()
 
 
-void usage_error (const char *filename);    // prints the proper usage of encoder.c
+void usage_error (const char *filename);    // print the proper usage of encoder.c
+
 
 int main(int argc, const char * argv[]) {
 
@@ -24,11 +25,11 @@ int main(int argc, const char * argv[]) {
     FILE *pcap_out;
     
     // Command-line arguments
-    if(argc > 3){                       // Check for more than one argument, error.
+    if(argc != 3){                       // Check for more than two arguments, error.
         error_n = errno;
         system("clear");
-        fprintf(stderr, "Error in opening %s: %s\n", argv[0], strerror(error_n));
-        printf("Usage: %s <path to text file> < path to destination PCAP file>\n (this will overwrite that file)", argv[0]);
+        fprintf(stderr, "Error running %s: %s\n", argv[0], strerror(error_n));
+        usage_error (*argv);
         return 7;                       // Argument List too Long.
     }else if(argc == 3){
         
@@ -39,23 +40,22 @@ int main(int argc, const char * argv[]) {
         // Check both files to validate they are there and accessable.
         if (text_input && pcap_out){
             printf("DEBUG: Your two file locations are good. Who knows if they are the correct types of files.\n");
-            exit(0);
-        }else if (!text_input){
+            
+        } else if (!text_input){
             printf("Error! No text file at '%s'.\n", argv[1]);
             usage_error (*argv);
-        }else if (!pcap_out){
+        } else if (!pcap_out){
             printf("Error! No destination PCAP file at '%s'\n", argv[2]);
             usage_error (*argv);
         }
         
-
     }
+    
     return 0;
 }
 
-// prints the proper usage of arguments in encoder.c
+// print the proper usage of arguments in encoder.c
 void usage_error (const char *filename){
-    printf("Usage: %s <path to text file> <path to destination PCAP file>\n (this will overwrite that file)\n", filename);
-    exit(1);
-    
+    printf("\nUsage: %s <path to text file> <path to destination PCAP file>\n (this will overwrite that file)\n", filename);
+    exit(7);
 };
