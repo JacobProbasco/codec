9a9d368e6892bf80ff018b122d90791bcc890472
JacobProbasco <jacobprobasco@gmail.com>
2015-12-06 04:18:30 -0500

    Add most of the logic for checking the line data

--- encoder.c
+++ encoder.c
@@ -7,6 +7,8 @@
 //  And now for some fun with mostly non-delimited text!
 
 #define _BSD_SOURCE
+#define ARR_SIZE(a) (sizeof(a) / sizeof(*a))
+
 #include <stdio.h>          // fileno()
 #include <string.h>         // memset() and strerror()
 #include <stdlib.h>         // system() and others
@@ -24,7 +26,9 @@ void set_ethernet(struct ethernet *);
 void set_IPv4(struct IPv4 *);
 void set_udp(struct UDP *);
 
+int find_word(int chosen_array, FILE *text_input);
 void usage_error (const char *filename);    // print the proper usage of encoder.c
+void exit_clean(FILE *, FILE *);
 
 
 int main(int argc, const char * argv[]) {
@@ -33,88 +37,51 @@ int main(int argc, const char * argv[]) {
     
     FILE *text_input;
     FILE *pcap_out = NULL;
-     // Command-line arguments
-     if(argc != 3){                       // Check for more than two arguments, error.
-     system("clear");
-     fprintf(stderr, "Error running %s: %s\n", argv[0], strerror(errno));
-     usage_error (*argv);
-     return 7;                       // Argument List too Long.
-     }
+    
+    // Command-line arguments
+    if(argc != 3){                       // Check for more than two arguments, error.
+        system("clear");
+        fprintf(stderr, "Error running %s: %s\n", argv[0], strerror(errno));
+        usage_error (*argv);
+        return 7;                       // Argument List too Long.
+    }
      
      // Open files as a data stream
-     text_input = fopen(argv[1], "rb");
-     pcap_out = fopen(argv[2], "w+b");       // Writeable so we can use it if good.
+    text_input = fopen(argv[1], "rb");
+    pcap_out = fopen(argv[2], "w+b");       // Writeable so we can use it if good.
     
     if(argc == 3){
         // Check both files to validate they are there and accessable.
         if (!text_input){
             printf("Error! No text file at '%s'.\n", argv[1]);
             usage_error (*argv);
+            exit_clean(pcap_out, text_input);
         } else if (!pcap_out){
             printf("Error! No destination PCAP file at '%s'\n", argv[2]);
             usage_error (*argv);
+            exit_clean(pcap_out, text_input);
         }
     }
     
     printf("DEBUG: Your two file locations are good.\nWho knows if they are the correct types of files. Here...We....GO...\n\n");
     
-    // Array of pointers for Network data structure
-    
-    char current_char;
-    current_char = fgetc(text_input);
-    
     // array of character arrays with the values for the default med_head
-    char words[5][10] = { "Type: ", "Version: ", "Squence: ", "From: ", "To: " };
-    
-    enum reset_values{ match_found, word };
-    enum reset_values reset;
     
     int line_count = 0;
     
     // Read the given text file.
     while(feof(text_input) == 0){
-        current_char = fgetc(text_input);
         line_count ++;
         
-        // Loop through words
-        for (int word = 0; word < 5; word++){
-            // Loop through each character in words
-            for (int letter = 0; letter != sizeof(words[word]); letter ++){
-                if (words[word][letter] != current_char){
-                    
-                }
-                printf("%c", words[word][letter]);
-                
-                if (letter == sizeof(words[word]) - 1){
-                    printf("\n");
-                }
-                
+        for (int i = 0; i <= 4; i++){
+            if (find_word(0, text_input) ){
+                printf("Invalid Data in Meditrick Header. Exiting.");
             }
+            
         }
         
-        
-/*
-        if ((check_end > 0) && (current_char != '*')){
-            check_end = 0;
-        }else if ((current_char == '*') && (char_count > 7)){
-            check_end++;
-            if (check_end >= 7){
-                printf("Text Error: ******* u");
-            }
-        }
- */
-        
-// END WHILE-LOOP
     }
-/*
-    if(fseek(text_input, 2, SEEK_CUR) == -1){
-        break;
-    }
-    
-    printf("%c\n", character);
-    
-    fseek(text_input, -2, SEEK_CUR);
-*/
+/* BEGIN GOOD CODE - PUT BACK
     struct global global;
     struct packet packet;
     struct ethernet ethernet;
@@ -127,14 +94,11 @@ int main(int argc, const char * argv[]) {
     set_IPv4(&IPv4);
     set_udp(&UDP);
     
-    /* // Could not get this working. Supposed to loop the writing.
-     
-     void *structures[5] = { &global, &packet, &ethernet, &IPv4, &UDP };
-     
-     for (int i = 0; i < 6; i++){
-     fwrite(structures[i], sizeof(*structures[i]), 1, pcap_out);
-     }
-     */
+    // Could not get this working. Supposed to loop the writing.
+    // void *structures[5] = { &global, &packet, &ethernet, &IPv4, &UDP };
+    // for (int i = 0; i < 6; i++){
+    //     fwrite(structures[i], sizeof(*structures[i]), 1, pcap_out);
+    // }
     
     fwrite(&global, sizeof(global), 1, pcap_out);
     fwrite(&packet, sizeof(packet), 1, pcap_out);
@@ -211,12 +175,9 @@ int main(int argc, const char * argv[]) {
         
     }
     
-    
     fclose(pcap_out);
     fclose(text_input);
-    
-    
-    
+*/
     /*
      for(int i = 0; i < 6; i++){
      printf("\n%p", &net_data + i);
@@ -225,21 +186,6 @@ int main(int argc, const char * argv[]) {
      
      set_global(net_data);
      */
-    
-    
-    
-    
-    
-    
-    
-    //***** Changes to data header
-    //***** get rid of chars
-    
-    // struct PCAP pcap_header;
-    
-    // FIXME: Make user file more descriptive and make this a part of the encode.
-    
-    
     
     /*
      unsigned int default_PCAP_header[24] = { '0xD4', '0xC3', 0xB2, 0xA1, 0x02, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00};
@@ -261,8 +207,7 @@ int main(int argc, const char * argv[]) {
      fwrite(default_network_frame+i, sizeof(*default_network_frame), 1, pcap_out);
      }
      */
-    
-    
+
     /*
      pcap_t *pd;
      pcap_dumper_t *pdumper;
@@ -280,8 +225,6 @@ int main(int argc, const char * argv[]) {
      pcap_close(pd);
      pcap_dump_close(pdumper);
      */
-    
-    
     
     /*
      // Meditrik header. - Maximum size of med_header is 24B
@@ -344,6 +287,53 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+int find_word(int chosen_array, FILE *text_input){
+    
+    // Lists of possible words
+    char words[5][10] = { "Type:", "Version:", "Squence:", "From:", "To:" };
+    char CMND_words[8][13] = { "GET_STATUS", "SET_GLUCOSE", "GET_GPS", "SET_CAPSAICIN", "RESERVED(4)","SET_OMORFINE", "RESERVED(6)", "REPEAT" };
+
+    void *word_array_list[2] = { &words[0], &CMND_words[0] };
+    char *given_array = &*word_array_list[chosen_array];
+    
+    char input_char;
+    
+    
+    int word;
+    int character = 0;
+    
+    // Loop through a given array, word by word
+    for (word = 0; word < ARR_SIZE(given_array); word++){
+        // Loop through each character in the word element
+        
+        for (; character != sizeof(*given_array + word); character++){
+            input_char = fgetc(text_input);
+            // if that character does not match the current word from the file
+            
+            if (*(given_array + word + character) != input_char){
+                character = 0;
+                // Rewind how many characters we've tried
+                fseek(text_input, -(long)character, SEEK_CUR);
+            }
+            
+            printf("%c", *(given_array + word + character));
+            
+        }
+        
+        if (character > 0){
+            printf("Word not valid\n");
+            // Return error code
+            return -1;
+        }
+        
+    }
+    
+    // if the program gets here, it did not find a valid word
+    printf("Invalid data in file. Exiting");
+    exit(0);
+};
+
+
 // print the proper usage of arguments in encoder.c
 void usage_error (const char *filename){
     printf("\nUsage: %s <path to text file> <path to destination PCAP file>\n (this will overwrite that file)\n", filename);
@@ -394,3 +384,10 @@ void set_udp(struct UDP *func_udp){
     func_udp->length = 0x0000;
     func_udp->chksum = 0x0000;
 }
+                
+void exit_clean(FILE * pcap_out, FILE * text_input){
+    fclose(pcap_out);
+    fclose(text_input);
+    exit(0);
+}
+
